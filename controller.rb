eb083a0502def85e59e1c0e80773d49bcd005f0b
Jayno17 <julia.nowak@gmail.com>
2020-02-04 15:57:14 +0000

    initial commit

---
+++ controller.rb
@@ -0,0 +1,11 @@
+require('sinatra')
+require('sinatra/contrib/all') if development?
+require('pry-byebug')
+
+require_relative('./models/film')
+also_reload('./models/*')
+
+get '/films'do
+  @films = Film.all()
+  erb(:index)
+end
